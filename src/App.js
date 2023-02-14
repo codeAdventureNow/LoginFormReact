66746cdf45ad17063a4033f367e06d2ef3a41827
[David <[nmueller855@gmail.com]>
2023-02-13 16:38:46 -0800

    completed witout styling, email input, password input, console.log user data, password longer than 4, no empy inputs, valid email address

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import { useState } from 'react';
 export default function MyForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
   function handleEmailChange(e) {
     e.preventDefault();
+
     setEmail(e.target.value);
   }
 
@@ -16,19 +19,39 @@ export default function MyForm() {
 
   function handleSubmit(e) {
     e.preventDefault();
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    setMessage('');
+
+    if (!regex.test(email)) {
+      setMessage('Please enter a valid email');
+    }
+
+    if (password.length < 5 || password === '') {
+      setMessage('Your password must be more than 4 characters');
+    }
+
+    if (email === '') {
+      setMessage('Please enter your email.');
+    }
+
     console.log(email, password);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email: <input value={email} onChange={handleEmailChange} type='text' />
-      </label>
-      <label>
-        Pasword:{' '}
-        <input value={password} onChange={handlePasswordChange} type='text' />
-      </label>
-      <button type='submit'>Enter</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:{' '}
+          <input value={email} onChange={handleEmailChange} type='text' />
+        </label>
+        <label>
+          Pasword:{' '}
+          <input value={password} onChange={handlePasswordChange} type='text' />
+        </label>
+        <button type='submit'>Enter</button>
+      </form>
+      <p> {message}</p>
+    </div>
   );
 }
